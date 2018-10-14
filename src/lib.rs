06cfe77569eda3c9c2e5f1f1c1f32c77bb0d73b2
Emyli Poltorak <emylipoltorak@gmail.com>
2018-10-14 13:05:28 -0700

    added a write posts feature that saves a draft.

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ use diesel::prelude::*;
 use diesel::pg::PgConnection;
 use dotenv::dotenv;
 use std::env;
+use self::models::{Post, NewPost};
 
 pub mod schema;
 pub mod models;
@@ -19,7 +20,19 @@ pub fn establish_connection() -> PgConnection {
         .expect(&format!("Error connecting to {}", database_url))
 }
 
+pub fn create_post<'a>(conn: &PgConnection, title: &'a str, body: &'a str) -> Post {
+    use self::schema::posts;
 
+    let new_post = NewPost {
+        title: title,
+        body: body,
+    };
+
+    diesel::insert_into(posts::table)
+        .values(&new_post)
+        .get_result(conn)
+        .expect("Error saving new post")
+}
 
 #[cfg(test)]
 mod tests {
